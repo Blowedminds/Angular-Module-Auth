c783504756d12f47ef485547fb7ec641a2432b1f
blowedminds <ruzgardeniz.08@hotmail.com>
2018-07-09 15:25:12 +0300

    route update

--- auth-routing.module.ts
+++ auth-routing.module.ts
@@ -1,22 +1,22 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes }    from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { LogoutComponent } from './components/logout/logout.component';
 
-const routes = [
-  { path: "", redirectTo: "login", pathMatch: "full" },
-  { path: "login",/* canActivate: [GuestRouteGuard],*/ component: LoginComponent },
-  { path: "register",/* canActivate: [GuestRouteGuard],*/ component: RegisterComponent },
-  { path: "logout", component: LogoutComponent }
+const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', /* canActivate: [GuestRouteGuard],*/ component: LoginComponent },
+  { path: 'register', /* canActivate: [GuestRouteGuard],*/ component: RegisterComponent },
+  { path: 'logout', component: LogoutComponent }
 ];
 
 @NgModule({
   imports: [
     RouterModule.forRoot(routes,
-      { enableTracing: false}
-  )],
+      { enableTracing: false }
+    )],
   exports: [
     RouterModule
   ]
